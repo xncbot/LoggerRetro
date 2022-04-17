451e42bc37413c510c5bd2879e7befea62fd11bf
xnc <57753840+xncbot@users.noreply.github.com>
2022-04-16 20:49:25 -0700

    first commit on setting up

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test this shit");
+    }
+}
